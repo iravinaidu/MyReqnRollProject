742fd84d233592a12651d4473835816db4acdbcf
mallisetti ravikumar <kumarcuty24@outlook.com>
2026-03-01 12:51:28 +0530

    added a new Reqnroll project

---
+++ ReqnrollQuickstart.Specs/Features/PriceCalculation.feature
@@ -0,0 +1,27 @@
+﻿#language: en
+
+Feature: Price calculation
+
+This feature is about calculating the basket price
+
+We work with fixed item prices for now:
+* Electric guitar: $180
+* Guitar pick: $1.5
+
+Rule: The price for a basket with items can be calculated based on the item prices
+
+Scenario: Client has a simple basket
+    Given the client started shopping
+    And the client added 1 pcs of "Electric guitar" to the basket
+    When the basket is prepared
+    Then the basket price should be $180.0
+
+Scenario: Client has a multiple basket
+    Given the client started shopping
+    And the client added
+    | product         | quantity  |
+    | Electric guitar | 1         |
+    | Guitar pick     | 10        |
+    When the basket is prepared
+    Then the basket price should be $195.0
+    But the basket price should not be %200.0
---
+++ ReqnrollQuickstart.Specs/StepDefinitions/PriceCalculationStepDefinitions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Reqnroll;
+
+namespace ReqnrollQuickstart.Specs.StepDefinitions
+{
+    [Binding]
+    public class PriceCalculationStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private readonly PriceCalculator _priceCalculator = new();
+        private readonly Dictionary<string,int> _basket= new();
+        private decimal _calculatedPrice ;
+
+        public PriceCalculationStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the client started shopping")]
+        public void GivenTheClientStartedShopping()
+        {
+            _basket.Clear();
+            _calculatedPrice = 0.0m;
+        }
+
+        [Given("the client added {int} pcs of {string} to the basket")]
+        public void GivenTheClientAddedPcsOfToTheBasket(int quantity, string product)
+        {
+            _basket.Add(product, quantity);
+        }
+
+        [Given("the client added")]
+        public void GivenTheClientAdded(DataTable itemsTable)
+        {
+            var items = itemsTable.CreateSet<(string product, int quantity)>();
+            foreach (var item in items)
+            {
+                _basket.Add(item.product, item.quantity);
+            }
+        }
+
+
+        [When("the basket is prepared")]
+        public void WhenTheBasketIsPrepared()
+        {
+            _calculatedPrice = _priceCalculator.CalculatePrice(_basket);
+        }
+
+        [Then("the basket price should be ${float}")]
+        public void ThenTheBasketPriceShouldBe(Decimal expectedPrice)
+        {
+            Assert.AreEqual(expectedPrice, _calculatedPrice);
+        }
+
+        [Then("the basket price should not be %{float}")]
+        public void ThenTheBasketPriceShouldNotBe(Decimal expectedPrice)
+        {
+            Assert.AreNotEqual(expectedPrice, _calculatedPrice);
+        }
+
+    }
+}
